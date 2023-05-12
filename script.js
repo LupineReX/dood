0fa13fd0ba299c9af11996fe581d68892b9cac5b
Timmy Nguyen <Timmy@WebDev03.local>
2023-05-12 10:48:09 -0400

    wow

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-function myFunction() {
-    document.getElementById("demo").innerHTML = "Hello World";
-  }
+let input = document.querySelector("input");
+const button = document.querySelector(cbtn);
 btn.addEventListener("onclick",myFunction);
+addEventListener("click",)
+function myFunction() {
+    console.log("click")
+}
